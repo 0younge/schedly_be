7915dc16da05ab9b9f6a98d06c06ade8c2a82f8b
0younge <s45628439@gmail.com>
2026-05-22 16:40:27 +0900

    chore: scaffold backend project

---
+++ src/test/java/com/schedly/api/SchedlyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedly.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedlyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
